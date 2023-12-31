74595da896367b7bcad8ccdbc0263df1bf5d57d0
amir-asgari <mr.a.asgari@gmail.com>
2023-12-31 14:39:44 +0330

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ControlFlowComponent } from './control-flow/control-flow-component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ControlFlowComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'exercise';
+  title: string = 'exercise';
 }
---
+++ src/app/control-flow/control-flow-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-control-flow',
+  templateUrl: './control-flow.component.html',
+})
+export class ControlFlowComponent {}
